1154377b8df5e3dcd1ce01c22fa1d755e83fa557
happystarEve <dongpingeve@126.com>
2020-04-03 16:45:58 +0800

    Update aliyunconfig.php
    删除密钥

--- sys/controller/aliyunconfig.php
+++ sys/controller/aliyunconfig.php
@@ -1,7 +1,7 @@
 <?php
 final class ALiYun_Config{
-	const OSS_ACCESS_ID = "OwStJvolsSv3lLEn";
-	const OSS_ACCESS_KEY = "mAOZQb8TS59vVMnSmC43zhT1QEdGCN";
-	const OSS_ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";
+	const OSS_ACCESS_ID = "";
+	const OSS_ACCESS_KEY = "";
+	const OSS_ENDPOINT = "";
 	const OSS_BUCKET = "eve-cloud";
 }
